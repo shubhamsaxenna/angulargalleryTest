ac198640aa98a80a95937663d8818d423c903b08
shubhamsaxenna <shubhamsaxenna@gmail.com>
2022-07-04 23:45:25 +0530

    gallery functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Lightbox } from 'ngx-lightbox';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularTest';
+    album: Array<any> = []; 
+  constructor(private _lightbox: Lightbox,) {
+    this.album = []
+    for (let i = 1; i <= 4; i++) {
+      const src = '../assets/' + i + '.png';
+      const caption = 'Image ' + i ;
+      const thumb = '../assets/' + i + '.png';
+      const album = {
+         src: src,
+         caption: caption,
+         thumb: thumb,
+         downloadUrl:src,
+         data:src
+      };
+
+      this.album.push(album);
+    }
+  }
+
+  
+  open(index: number): void {
+    this._lightbox.open(this.album, index,{ wrapAround: false, showZoom: true, showRotate: true, centerVertically: true, fitImageInViewPort:true});
+    // this._lightbox.setConfig({thumbPosition: ThumbnailsPosition.Top,})
+  }
+
+  close(): void {
+    this._lightbox.close();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LightboxModule } from 'ngx-lightbox';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LightboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
